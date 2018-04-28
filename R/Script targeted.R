0ffaaa6b1ec59d049aea70ae6f96ae266bb71cf0
sontis3 <oleg.ovcharenko@labworks.ru>
2018-04-28 14:07:32 +0300

    .

--- R/Script targeted.R
+++ R/Script targeted.R
@@ -1,20 +1,23 @@
 library("readxl", lib.loc="~/R/win-library/3.4")
 # SerumAll <- read_excel("Z:/UBUNTU/00 - Личные папки сотрудников/13-Маркин/РПЖ/Сводные таблицы/r/SerumAll.xlsx", sheet = "SerumAll")
 # View(SerumAll)
-pc <- read.csv("z:/UBUNTU/00 - Личные папки сотрудников/13-Маркин/РПЖ/Сводные таблицы/r/pc.csv", header = T, sep = ",", check.names = F)
-ph <- read.csv("z:/UBUNTU/00 - Личные папки сотрудников/13-Маркин/РПЖ/Сводные таблицы/r/ph.csv", header = T, sep = ",", check.names = F)
+pc <- read.csv("z:/UBUNTU/00 - Личные папки сотрудников/13 - Маркин/РПЖ/Сводные таблицы/r/PC Serum.csv", header = T, sep = ",", check.names = F)
+ph <- read.csv("z:/UBUNTU/00 - Личные папки сотрудников/13 - Маркин/РПЖ/Сводные таблицы/r/PH Serum.csv", header = T, sep = ",", check.names = F)
 
-tt <- sapply(colnames(pc)[-1:-2], function(name) t.test(pc[name], ph[name]))
-ttl <- lapply(colnames(pc)[-1:-2], function(name) t.test(pc[name], ph[name]))
+tt <- sapply(colnames(pc)[-1:-1], function(name) t.test(pc[name], ph[name]))
+# ttl <- lapply(colnames(pc)[-1:-2], function(name) t.test(pc[name], ph[name]))
 fdr_tt <-p.adjust(tt["p.value",], method = "BH")
 
 
-mwt <- sapply(colnames(pc)[-1:-2], function(name) wilcox.test(pc[[name]], ph[[name]]))
-mwl <- lapply(colnames(pc)[-1:-2], function(name) wilcox.test(pc[[name]], ph[[name]]))
+mwt <- sapply(colnames(pc)[-1:-1], function(name) wilcox.test(pc[[name]], ph[[name]]))
+# mwl <- lapply(colnames(pc)[-1:-2], function(name) wilcox.test(pc[[name]], ph[[name]]))
 fdr_mwt <-p.adjust(mwt["p.value",], method = "BH")
 
 
-write.csv(tt, "aaa.csv")
+write.csv(tt, "tt.csv")
+write.csv(mwt, "mwt.csv")
+write.csv(fdr_tt, "fdr_tt.csv")
+write.csv(fdr_mwt, "fdr_mwt.csv")
 
 tt[,1:3]
 colnames(tt)
